c0b6a7690e561b271bfd17e86ea25ff4c2b81d62
qonehan <kyuwonhan04@gmail.com>
2024-08-07 19:55:18 +0900

    shared_preferences

--- flutter_application_a/lib/main.dart
+++ flutter_application_a/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_a/mainscreen.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   runApp(const MyApp());
 }
